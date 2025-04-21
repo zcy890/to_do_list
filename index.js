03be82cf4a45a99d2934a6d96856aa33a1e3b386
Chris Yan <zcy890@gmail.com>
2025-04-20 23:03:49 -0700

    remove redundant var assigning

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ app.use(express.static("public"));
 app.get("/", async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM items ORDER BY id ASC");
-    items = result.rows;
     console.log(items);
 
     res.render("index.ejs", {
